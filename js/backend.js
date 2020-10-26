1f79ea757729f2e84b40908cb44dcbc7ae2d8cdb
ElizabethSh <liza.shoshkina@gmail.com>
2020-10-26 14:30:05 +0100

    Remove comment

--- js/backend.js
+++ js/backend.js
@@ -6,7 +6,7 @@
   const Code = {
     SUCCESS: 200,
     BAD_REQUEST: 400,
-    FORBIDDEN: 403, // проверить название
+    FORBIDDEN: 403,
     NOT_FOUND: 404
   };
 
@@ -57,5 +57,4 @@
   window.backend = {
     load
   };
-
 })();
