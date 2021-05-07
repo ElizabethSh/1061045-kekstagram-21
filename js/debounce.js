0c1ecb744669437349c988ad5922baba64bea49b
ElizabethSh <liza.shoshkina@gmail.com>
2021-05-07 19:54:13 +0200

    Refactoring

--- js/debounce.js
+++ js/debounce.js
@@ -1,15 +1,16 @@
 'use strict';
 
 (function () {
-  const TIMEOUT = 500;
-
+  const DEBOUNCE_INTERVAL = 500;
   let timeout;
+
   window.debounce = (cb) => {
     if (timeout) {
       window.clearTimeout(timeout);
     }
+
     timeout = window.setTimeout(() => {
       cb();
-    }, TIMEOUT);
+    }, DEBOUNCE_INTERVAL);
   };
 })();
