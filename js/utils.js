8b8940316855757540ea9f587eb63df15c544d50
ElizabethSh <liza.shoshkina@gmail.com>
2021-05-07 19:52:43 +0200

    Refactoring

--- js/utils.js
+++ js/utils.js
@@ -1,8 +1,12 @@
 'use strict';
 
 (function () {
+  const KeyCode = {
+    ESC_KEY: `Escape`,
+  };
+
   const isEscapeEvent = (evt, action) => {
-    if (evt.key === `Escape`) {
+    if (evt.key === KeyCode.ESC_KEY) {
       evt.preventDefault();
       action();
     }
